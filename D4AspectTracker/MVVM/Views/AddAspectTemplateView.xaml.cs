fb06f222966efe086dd3816a1e82abd5cf71fc19
tylerharrold <tylerharrold@gmail.com>
2023-07-12 20:55:08 -0400

    Added method to ensure entry of all required data

--- D4AspectTracker/MVVM/Views/AddAspectTemplateView.xaml.cs
+++ D4AspectTracker/MVVM/Views/AddAspectTemplateView.xaml.cs
@@ -1,7 +1,12 @@
+using System.Diagnostics;
+
 namespace D4AspectTracker.MVVM.Views;
 
 public partial class AddAspectTemplateView : ContentPage
 {
+
+    private string _aspectType;
+
 	public AddAspectTemplateView()
 	{
 		InitializeComponent();
@@ -9,6 +14,7 @@ public partial class AddAspectTemplateView : ContentPage
 
     private void RadioRangedRoll_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
+        _aspectType = "rangedrollvalue";
         TurnOnAddAspectButton();
         RangedRollEntry.IsVisible = e.Value;
         if (!e.Value)
@@ -21,13 +27,15 @@ public partial class AddAspectTemplateView : ContentPage
 
     private void RadioSetValue_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
+        _aspectType = "setvalue";
         TurnOnAddAspectButton();
         
     }
 
     private void RadioStaticValue_CheckedChanged(object sender, CheckedChangedEventArgs e)
     {
-        TurnOnAddAspectButton();
+        _aspectType = "staticvalue";
+        TurnOnAddAspectButton(); 
         StaticValueEntry.IsVisible = e.Value;
         if(!e.Value)
         {
@@ -36,13 +44,20 @@ public partial class AddAspectTemplateView : ContentPage
 
     }
 
-    private void AddAspectButton_Clicked(object sender, EventArgs e)
+    private async void AddAspectButton_Clicked(object sender, EventArgs e)
     {
-        // ensure all required data is entered
+        // ensure all required data is entered, pop warning if not
+        if (!IsAllDataEntered())
+        {
+            await DisplayAlert("Required Fields Missing", "Please enter all required fields.", "OK");
+        }
+        else
+        {
+            // if everything is all set we call to AddAspectViewModel.AddPerson() to add to db
+        }
 
-        // pop up alert if necessary information is missing
 
-        // if everything is all set we call to AddAspectViewModel.AddPerson() to add to db
+
 
         // pop error message if we get error from db entry
     }
@@ -53,5 +68,43 @@ public partial class AddAspectTemplateView : ContentPage
         {
             AddAspectButton.IsVisible = true;
         }
+    }
+
+    // method for ensuring all data fields have been properly entered before triggering
+    // a call for db insertion
+    private bool IsAllDataEntered()
+    {
+        bool allDataEntered = false;
+
+        // aspect name, aspect category, aspect type
+        if (string.IsNullOrEmpty(AspectNameEntry.Text) ||
+            AspectCategoryPicker.SelectedIndex < 0 ||
+            string.IsNullOrEmpty(_aspectType))
+        {
+            return allDataEntered;
+        }
+       
+        switch (_aspectType)
+        {
+            case "rangedrollvalue":
+                if(string.IsNullOrEmpty(MinRollValue.Text) || string.IsNullOrEmpty(MaxRollValue.Text))
+                {
+                    return allDataEntered;
+                }
+                break;
+            case "staticvalue":
+                if (string.IsNullOrEmpty(StaticValue.Text))
+                {
+                    return allDataEntered;
+                }
+                break;
+            default:
+                break;
+        }
+
+        // all data has been entered
+        allDataEntered = true;
+
+        return allDataEntered;     
     }
 }
---
+++ D4AspectTracker/Utility/FileAccessHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace D4AspectTracker.Utility
+{
+    public class FileAccessHelper
+    {
+        public static string GetLocalFilePath(string fileName)
+        {
+            return System.IO.Path.Combine(FileSystem.AppDataDirectory, fileName);
+        }
+    }
+}
