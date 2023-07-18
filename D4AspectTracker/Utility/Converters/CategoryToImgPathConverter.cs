7c3354dd752e3f0b43eea900429c821a8133382e
tylerharrold <tylerharrold@gmail.com>
2023-07-18 15:53:54 -0400

    Implemented custom value converter to be able to dynamically assign an image to an item from xaml views

---
+++ D4AspectTracker/Utility/Converters/CategoryToImgPathConverter.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Globalization;
+using System.Xml;
+
+namespace D4AspectTracker.Utility.Converters
+{
+    internal class CategoryToImgPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int categoryValue = (int)value;
+
+            switch(categoryValue)
+            {
+                case 0:
+                    return "offensive_aspect.png";
+                case 1:
+                    return "defensive_aspect.png";
+                case 2:
+                    return "resource_aspect.png";
+                case 3:
+                    return "utility_aspect.png";
+                case 4:
+                    return "mobility_aspect.png";
+                default:
+                    return "offensive_aspect.png";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
