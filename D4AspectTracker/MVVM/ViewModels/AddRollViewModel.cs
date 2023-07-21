e372f2e7498cc51323aaf5a60d76243fe1e364c3
tylerharrold <tylerharrold@gmail.com>
2023-07-21 14:20:00 -0400

    Began work on Aspect search feature

---
+++ D4AspectTracker/MVVM/ViewModels/AddRollViewModel.cs
@@ -0,0 +1,43 @@
+﻿using D4AspectTracker.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D4AspectTracker.MVVM.ViewModels
+{
+    class AddRollViewModel
+    {
+
+        public ObservableCollection<D4Aspect> Aspects;
+
+        public AddRollViewModel()
+        {
+            // grab all aspects 
+            Aspects = new ObservableCollection<D4Aspect>(App.DBManager.GetAllD4Aspects());
+        }
+
+        public void OnSearchButtonPressed(object sender, EventArgs e)
+        {
+            SearchBar sb = (SearchBar)sender;
+
+            // search the aspects collection for an aspect matching the name 
+            foreach(D4Aspect aspect in Aspects)
+            {
+                if(aspect.AspectName == sb.Text)
+                {
+                    Debug.Print($"Found {aspect.AspectName}");
+                    break;
+                }
+            }
+
+        }
+
+
+    }
+
+    
+}
--- D4AspectTracker/MVVM/Views/AddRollView.xaml.cs
+++ D4AspectTracker/MVVM/Views/AddRollView.xaml.cs
@@ -1,9 +1,20 @@
+using D4AspectTracker.MVVM.ViewModels;
+
 namespace D4AspectTracker.MVVM.Views;
 
 public partial class AddRollView : ContentPage
 {
+	private AddRollViewModel _viewModel;
+
 	public AddRollView()
 	{
 		InitializeComponent();
+
+		// hook up to viewmodel
+		_viewModel = new AddRollViewModel();
+		BindingContext = _viewModel;
+
+		// bind search bar to event handler in viewmodel
+		schAspect.SearchButtonPressed += _viewModel.OnSearchButtonPressed;
 	}
 }
